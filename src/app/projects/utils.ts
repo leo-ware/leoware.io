03a6bddb5f64addad0fb472972b9a1fb1e1d0aa2
Leo Ware <leoware@uni.minerva.edu>
2024-05-02 12:52:50 -0400

    troubleshoot file lookup issue

--- src/app/projects/utils.ts
+++ src/app/projects/utils.ts
@@ -1,13 +1,17 @@
 import parseMd from "parse-md"
 import fs from "fs"
+import path from "path"
 
 export const mdRoot = "src/projects"
 
 export const parseProjectMd = (fname: string) => {
     fname = fname.endsWith(".md") ? fname : fname + ".md"
-    const filePath = `${mdRoot}/${fname}`
-    const fileContents = fs.readFileSync(filePath, {encoding: 'utf8'})
     const slug = fname.replace(".md", "")
+
+    const filePath = path.join(process.cwd(), mdRoot, fname)
+    // const filePath = `${mdRoot}/${fname}`
+    const fileContents = fs.readFileSync(filePath, {encoding: 'utf8'})
+    
     const {metadata, content} = parseMd(fileContents) as {
         content: string,
         metadata?: {
